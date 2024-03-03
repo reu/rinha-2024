bba006f319b8a3e655b7586c2e1e5fa1941191cc
Rodrigo Navarro <rnavarro1@gmail.com>
2024-03-03 12:00:04 -0300

    Primeira versão da persistencia do EsporaDB

--- espora-db/src/lib.rs
+++ espora-db/src/lib.rs
@@ -1,15 +1,18 @@
 #![allow(unused)]
 use std::{
     error::Error,
-    io::{self, Write}, iter,
+    fs::{File, OpenOptions},
+    io::{self, Read, Seek, Write},
+    iter,
+    marker::PhantomData,
+    path::Path,
 };
 
-use serde::Serialize;
+use serde::{de::DeserializeOwned, Serialize};
 
 const PAGE_SIZE: usize = 4096;
-const ROW_SIZE: usize = 256;
 
-struct Page {
+struct Page<const ROW_SIZE: usize = 64> {
     data: Vec<u8>,
 }
 
@@ -18,11 +21,18 @@ pub enum DbError {
     Serialize(Box<dyn Error>),
 }
 
-impl Page {
+impl<const ROW_SIZE: usize> Page<ROW_SIZE> {
     pub fn new() -> Self {
         Self {
             data: Vec::with_capacity(PAGE_SIZE),
         }
+    }
+
+    pub fn from_bytes(data: Vec<u8>) -> Result<Self, &'static str> {
+        if data.len() != PAGE_SIZE {
+            return Err("Invalid data size");
+        }
+        Ok(Self { data })
     }
 
     pub fn insert<S: Serialize>(&mut self, row: S) -> Result<(), DbError> {
@@ -40,12 +50,12 @@ impl Page {
         Ok(())
     }
 
-    pub fn rows(&self) -> impl Iterator<Item = Vec<u8>> + '_ {
+    pub fn rows(&self) -> impl Iterator<Item = &[u8]> + '_ {
         let mut cursor = 0;
         iter::from_fn(move || {
             let offset = cursor * ROW_SIZE;
             if offset + ROW_SIZE > self.data.len() {
-                return None
+                return None;
             }
 
             let row = &self.data[offset..offset + ROW_SIZE];
@@ -56,28 +66,138 @@ impl Page {
             };
 
             cursor += 1;
-            Some(row[8..8 + size].to_vec())
+            Some(&row[8..8 + size])
+        })
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn available_rows(&self) -> usize {
+        (PAGE_SIZE - self.data.len()) / ROW_SIZE
+    }
+}
+
+impl<const ROW_SIZE: usize> AsRef<[u8]> for Page<ROW_SIZE> {
+    fn as_ref(&self) -> &[u8] {
+        &self.data
+    }
+}
+
+pub struct Db<T, const ROW_SIZE: usize = 64> {
+    current_page: Page<ROW_SIZE>,
+    reader: File,
+    writer: File,
+    data: PhantomData<T>,
+}
+
+impl<const ROW_SIZE: usize, T: Serialize + DeserializeOwned> Db<T, ROW_SIZE> {
+    pub fn from_path(path: impl AsRef<Path>) -> io::Result<Self> {
+        let file = OpenOptions::new().write(true).create(true).open(&path)?;
+
+        Ok(Self {
+            current_page: Page::new(),
+            reader: File::open(&path)?,
+            writer: file,
+            data: PhantomData,
+        })
+    }
+
+    pub fn insert(&mut self, row: T) -> Result<(), DbError> {
+        self.current_page.insert(row);
+
+        // TODO: fazer um único write
+        self.writer.write(self.current_page.as_ref());
+        self.writer
+            .write(&vec![0; PAGE_SIZE - self.current_page.len()]);
+
+        if self.current_page.available_rows() == 0 {
+            self.current_page = Page::new();
+        } else {
+            self.writer.seek(io::SeekFrom::End(-(PAGE_SIZE as i64)));
+        }
+
+        Ok(())
+    }
+
+    fn pages(&mut self) -> impl Iterator<Item = Page> + '_ {
+        let mut cursor = 0;
+        iter::from_fn(move || {
+            let offset = (cursor * PAGE_SIZE) as u64;
+
+            if self.reader.seek(io::SeekFrom::Start(offset)).is_err() {
+                return None;
+            }
+
+            let mut buf = vec![0; PAGE_SIZE];
+            cursor += 1;
+            match self.reader.read_exact(&mut buf) {
+                Ok(()) => Some(Page::from_bytes(buf).unwrap()),
+                Err(_) => None,
+            }
+        })
+    }
+
+    pub fn rows(&mut self) -> impl Iterator<Item = T> + '_ {
+        self.pages().flat_map(|page| {
+            page.rows()
+                .filter_map(|row| bitcode::deserialize(row).ok())
+                .collect::<Vec<_>>()
         })
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use tempfile::tempdir;
+
     use super::*;
 
     #[test]
     fn test_insert_into_page() {
-        let mut page = Page::new();
+        let mut page = Page::<1024>::new();
+        assert_eq!(4, page.available_rows());
         page.insert(String::from("Rinha"));
+        assert_eq!(3, page.available_rows());
         page.insert(String::from("de"));
+        assert_eq!(2, page.available_rows());
         page.insert(String::from("Backend"));
+        assert_eq!(1, page.available_rows());
         page.insert(2024 as u64);
+        assert_eq!(0, page.available_rows());
 
         let mut rows = page.rows();
-        assert_eq!("Rinha", bitcode::deserialize::<String>(&rows.next().unwrap()).unwrap());
-        assert_eq!("de", bitcode::deserialize::<String>(&rows.next().unwrap()).unwrap());
-        assert_eq!("Backend", bitcode::deserialize::<String>(&rows.next().unwrap()).unwrap());
-        assert_eq!(2024, bitcode::deserialize::<u64>(&rows.next().unwrap()).unwrap());
+        assert_eq!(
+            "Rinha",
+            bitcode::deserialize::<String>(&rows.next().unwrap()).unwrap()
+        );
+        assert_eq!(
+            "de",
+            bitcode::deserialize::<String>(&rows.next().unwrap()).unwrap()
+        );
+        assert_eq!(
+            "Backend",
+            bitcode::deserialize::<String>(&rows.next().unwrap()).unwrap()
+        );
+        assert_eq!(
+            2024,
+            bitcode::deserialize::<u64>(&rows.next().unwrap()).unwrap()
+        );
+        assert!(rows.next().is_none());
+    }
+
+    #[test]
+    fn test_insert_into_db() {
+        let tmp = tempdir().unwrap();
+        let mut db = Db::<(i64, String)>::from_path(tmp.path().join("test.espora")).unwrap();
+
+        db.insert((50, String::from("Primeira")));
+        db.insert((-20, String::from("Segunda")));
+
+        let mut rows = db.rows();
+        assert_eq!((50, String::from("Primeira")), rows.next().unwrap());
+        assert_eq!((-20, String::from("Segunda")), rows.next().unwrap());
         assert!(rows.next().is_none());
     }
 }
