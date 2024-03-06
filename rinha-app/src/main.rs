bd7f141409de1b214f4a1ec2716a37393f915907
Rodrigo Navarro <rnavarro1@gmail.com>
2024-03-05 22:08:46 -0300

    Instancias do app implementados via proxy TCP.

---
+++ rinha-app/src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+
+use tokio::{
+    io,
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|port| port.parse::<u16>().ok())
+        .unwrap_or(9998);
+
+    let db = env::var("DB").unwrap_or(String::from("0.0.0.0:9999"));
+    let db: &'static str = Box::leak(db.into_boxed_str());
+
+    let listener = TcpListener::bind(("0.0.0.0", port)).await?;
+
+    println!("App ready {port}");
+
+    while let Ok((mut downstream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            let mut upstream = TcpStream::connect(db).await.unwrap();
+            io::copy_bidirectional(&mut downstream, &mut upstream)
+                .await
+                .unwrap();
+        });
+    }
+
+    Ok(())
+}
